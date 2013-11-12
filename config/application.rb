c8db3ce84a4b35fc9a4442d1e8580e3b14f2c347
Benjamin Oakes <boakes@continuity.net>
2013-11-12 14:01:58 -0500

    remove turbolinks, update gemset for jruby, add rspec

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,19 @@ module DonorsChoose
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    #
+    config.generators do |g|
+      g.orm :active_record
+      g.template_engine :erb
+      g.test_framework :rspec,
+        :fixture => false,
+        :views => false,
+        :view_specs => false,
+        :controller_specs => false,
+        :routing_specs => false
+      g.stylesheets false
+      g.javascripts false
+      g.helper = false
+    end
   end
 end
