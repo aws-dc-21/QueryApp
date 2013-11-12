d2963d19502f4421b622aff0398c63771a7af053
Bill Gathen and Benjamin Oakes <dev+bgathen+boakes@continuity.net>
2013-11-12 17:59:22 -0500

    (m) Remove generated comments

--- app/controllers/saved_queries_controller.rb
+++ app/controllers/saved_queries_controller.rb
@@ -1,28 +1,20 @@
 class SavedQueriesController < ApplicationController
   before_action :set_saved_query, only: [:show, :edit, :update, :destroy]
 
-  # GET /saved_queries
-  # GET /saved_queries.json
   def index
     @saved_queries = SavedQuery.all
   end
 
-  # GET /saved_queries/1
-  # GET /saved_queries/1.json
   def show
   end
 
-  # GET /saved_queries/new
   def new
     @saved_query = SavedQuery.new
   end
 
-  # GET /saved_queries/1/edit
   def edit
   end
 
-  # POST /saved_queries
-  # POST /saved_queries.json
   def create
     @saved_query = SavedQuery.new(saved_query_params)
 
@@ -37,8 +29,6 @@ class SavedQueriesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /saved_queries/1
-  # PATCH/PUT /saved_queries/1.json
   def update
     respond_to do |format|
       if @saved_query.update(saved_query_params)
@@ -51,8 +41,6 @@ class SavedQueriesController < ApplicationController
     end
   end
 
-  # DELETE /saved_queries/1
-  # DELETE /saved_queries/1.json
   def destroy
     @saved_query.destroy
     respond_to do |format|
@@ -62,13 +50,12 @@ class SavedQueriesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_saved_query
-      @saved_query = SavedQuery.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def saved_query_params
-      params.require(:saved_query).permit(:name, :description, :sql)
-    end
+  def set_saved_query
+    @saved_query = SavedQuery.find(params[:id])
+  end
+
+  def saved_query_params
+    params.require(:saved_query).permit(:name, :description, :sql)
+  end
 end
